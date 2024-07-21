97c00fcd6ccda2916e62643bee3f53dcc482f76e
Mondev <ludgianpichepaz@gmail.com>
2024-07-21 09:02:38 -0600

    logo redstone

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <img alt="RedStone Logo" src="../assets/LogoRedStone.png">
+    <HelloWorld msg="Bienvenido a RedStone"/>
   </div>
 </template>
 
